07a95f098947c932037d525a4335f232ec0ebf11
JumpingPxl <jumpingpxl@gmail.com>
2021-12-23 02:46:44 +0100

    Add NameTags Navigation Tab

--- core/src/main/java/net/labymod/addons/nametags/NameTags.java
+++ core/src/main/java/net/labymod/addons/nametags/NameTags.java
@@ -1,7 +1,10 @@
 package net.labymod.addons.nametags;
 
 import com.google.inject.Singleton;
+import javax.inject.Inject;
+import net.labymod.addons.nametags.gui.NameTagNavigationElement;
 import net.labymod.api.LabyAPI;
+import net.labymod.api.client.gui.navigation.NavigationRegistry;
 import net.labymod.api.configuration.loader.ConfigurationLoader;
 import net.labymod.api.configuration.settings.SettingsRegistry;
 import net.labymod.api.configuration.settings.gui.SettingCategoryRegistry;
@@ -11,8 +14,6 @@ import net.labymod.api.event.client.lifecycle.GameInitializeEvent;
 import net.labymod.api.event.client.lifecycle.GameInitializeEvent.Lifecycle;
 import net.labymod.api.event.labymod.config.ConfigurationSaveEvent;
 import net.labymod.api.models.addon.annotation.AddonMain;
-
-import javax.inject.Inject;
 
 @AddonMain
 @Singleton
@@ -40,14 +41,19 @@ public class NameTags {
       return;
     }
 
-    ConfigurationLoader configurationLoader = labyAPI.getConfigurationLoader();
+    ConfigurationLoader configurationLoader = this.labyAPI.getConfigurationLoader();
     try {
-      configuration = configurationLoader.load(NameTagConfiguration.class);
-      SettingsRegistry registry = configuration.initializeRegistry();
-      categoryRegistry.register(registry.getId(), registry);
+      this.configuration = configurationLoader.load(NameTagConfiguration.class);
+      SettingsRegistry registry = this.configuration.initializeRegistry();
+
+      this.categoryRegistry.register(registry.getId(), registry);
     } catch (Exception e) {
       e.printStackTrace();
     }
+
+    NavigationRegistry navigationRegistry = this.labyAPI.getNavigationService();
+    navigationRegistry.register("nametags",
+        this.labyAPI.getInjected(NameTagNavigationElement.class));
   }
 
   /**
---
+++ core/src/main/java/net/labymod/addons/nametags/gui/activity/NameTagActivity.java
@@ -0,0 +1,23 @@
+package net.labymod.addons.nametags.gui.activity;
+
+import net.labymod.api.client.gui.screen.Parent;
+import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
+import net.labymod.api.client.gui.screen.activity.types.extension.ListExtension;
+import net.labymod.api.client.gui.screen.activity.types.extension.NavigationExtension;
+
+public class NameTagActivity extends SimpleActivity implements NavigationExtension, ListExtension {
+
+  public NameTagActivity() {
+  }
+
+  @Override
+  public void initialize(Parent parent) {
+    super.initialize(parent);
+  }
+
+  @Override
+  public void initializeExtension() {
+    NavigationExtension.super.initializeExtension();
+    ListExtension.super.initializeExtension();
+  }
+}
