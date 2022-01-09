6a58ab89737c66c3380220452d36cdbb8485d2b9
JumpingPxl <jumpingpxl@gmail.com>
2022-01-09 01:59:07 +0100

    Add Remove Functionality to Name Tag Overview

--- core/src/main/java/net/labymod/addons/nametags/gui/activity/NameTagActivity.java
+++ core/src/main/java/net/labymod/addons/nametags/gui/activity/NameTagActivity.java
@@ -1,7 +1,7 @@
 package net.labymod.addons.nametags.gui.activity;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import javax.inject.Inject;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
@@ -12,6 +12,7 @@ import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
 import net.labymod.api.client.gui.screen.key.InputType;
 import net.labymod.api.client.gui.screen.key.Key;
 import net.labymod.api.client.gui.screen.key.MouseButton;
+import net.labymod.api.client.gui.screen.widget.Widget;
 import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.DivWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.input.ButtonWidget;
@@ -26,49 +27,35 @@ public class NameTagActivity extends SimpleActivity {
 
   private final NameTagConfiguration nameTagConfiguration;
   private final VerticalListWidget<NameTagWidget> nameTagList;
-  private final List<NameTagWidget> nameTagWidgets;
+  private final Map<String, NameTagWidget> nameTagWidgets;
 
   private ButtonWidget removeButton;
   private ButtonWidget editButton;
 
   private FlexibleContentWidget inputWidget;
-  private TextFieldWidget nameTextField;
-  private TextFieldWidget customTextField;
   private String lastUserName;
   private String lastCustomName;
 
   private Action action;
-  private NameTagWidget selectedNameTag;
 
   @Inject
   public NameTagActivity(NameTagConfiguration nameTagConfiguration) {
     this.nameTagConfiguration = nameTagConfiguration;
 
-    this.nameTagWidgets = new ArrayList<>();
+    this.nameTagWidgets = new HashMap<>();
     nameTagConfiguration.getCustomTags().forEach((userName, customTag) -> {
-      this.nameTagWidgets.add(new NameTagWidget(userName, customTag));
+      this.nameTagWidgets.put(userName, new NameTagWidget(userName, customTag));
     });
 
     this.nameTagList = new VerticalListWidget<>();
     this.nameTagList.addId("name-tag-list");
     this.nameTagList.setSelectCallback(nameTagWidget -> {
-      if (Objects.isNull(this.selectedNameTag)
-          || this.selectedNameTag.getCustomTag() != nameTagWidget.getCustomTag()) {
-        this.selectedNameTag = nameTagWidget;
+      NameTagWidget selectedNameTag = this.nameTagList.getSession().getSelectedEntry();
+      if (Objects.isNull(selectedNameTag)
+          || selectedNameTag.getCustomTag() != nameTagWidget.getCustomTag()) {
         this.editButton.setEnabled(true);
         this.removeButton.setEnabled(true);
       }
-    });
-
-    this.nameTagList.setDoubleClickCallback(nameTagWidget -> {
-      if (Objects.nonNull(this.action)) {
-        return;
-      }
-
-      this.selectedNameTag = nameTagWidget;
-      this.action = Action.EDIT;
-
-      this.reload();
     });
   }
 
@@ -79,27 +66,22 @@ public class NameTagActivity extends SimpleActivity {
 
     DivWidget listContainer = new DivWidget();
     listContainer.addId("name-tag-container");
-    for (NameTagWidget nameTagWidget : this.nameTagWidgets) {
-      if (nameTagWidget.getUserName().length() != 0) {
-        this.nameTagList.addChild(nameTagWidget);
-      }
+    for (NameTagWidget nameTagWidget : this.nameTagWidgets.values()) {
+      this.nameTagList.addChild(nameTagWidget);
     }
-
- /*   for (Entry<String, CustomTag> entry : this.nameTagConfiguration.getCustomTags().entrySet()) {
-      this.nameTagList.addChild(new NameTagWidget(entry.getKey(), entry.getValue()));
-    } */
 
     listContainer.addChild(new ScrollWidget(this.nameTagList));
     this.getDocument().addChild(listContainer);
 
+    NameTagWidget selectedNameTag = this.nameTagList.getSession().getSelectedEntry();
     HorizontalListWidget menu = new HorizontalListWidget();
     menu.addId("overview-button-menu");
 
     menu.addEntry(ButtonWidget.i18n("labymod.ui.button.add", () -> this.setAction(Action.ADD)));
     menu.addEntry(this.editButton = ButtonWidget.i18n("labymod.ui.button.edit",
-        () -> this.setAction(Action.EDIT)).setEnabled(Objects.nonNull(this.selectedNameTag)));
+        () -> this.setAction(Action.EDIT)).setEnabled(Objects.nonNull(selectedNameTag)));
     menu.addEntry(this.removeButton = ButtonWidget.i18n("labymod.ui.button.remove",
-        () -> this.setAction(Action.REMOVE)).setEnabled(Objects.nonNull(this.selectedNameTag)));
+        () -> this.setAction(Action.REMOVE)).setEnabled(Objects.nonNull(selectedNameTag)));
 
     this.getDocument().addChild(menu);
     if (Objects.isNull(this.action)) {
@@ -108,18 +90,18 @@ public class NameTagActivity extends SimpleActivity {
     DivWidget manageContainer = new DivWidget();
     manageContainer.addId("manage-container");
 
-    DivWidget overlayWidget;
+    Widget overlayWidget;
     switch (this.action) {
       default:
       case ADD:
         NameTagWidget newCustomNameTag = new NameTagWidget("", CustomTag.createDefault());
-        overlayWidget = this.initializeEditContainer(newCustomNameTag);
+        overlayWidget = this.initializeManageContainer(newCustomNameTag);
         break;
       case EDIT:
-        overlayWidget = this.initializeEditContainer(this.selectedNameTag);
+        overlayWidget = this.initializeManageContainer(selectedNameTag);
         break;
       case REMOVE:
-        overlayWidget = this.initializeEditContainer(this.selectedNameTag);
+        overlayWidget = this.initializeRemoveContainer(selectedNameTag);
         break;
     }
 
@@ -127,7 +109,36 @@ public class NameTagActivity extends SimpleActivity {
     this.getDocument().addChild(manageContainer);
   }
 
-  private DivWidget initializeEditContainer(NameTagWidget nameTagWidget) {
+  private FlexibleContentWidget initializeRemoveContainer(NameTagWidget nameTagWidget) {
+    this.inputWidget = new FlexibleContentWidget(true);
+    this.inputWidget.addId("remove-container");
+
+    ComponentWidget confirmationWidget = ComponentWidget.i18n("nametags.gui.manage.remove.title");
+    confirmationWidget.addId("remove-confirmation");
+    this.inputWidget.addContent(confirmationWidget);
+
+    NameTagWidget previewWidget = new NameTagWidget(nameTagWidget.getUserName(),
+        nameTagWidget.getCustomTag());
+    previewWidget.addId("remove-preview");
+    this.inputWidget.addContent(previewWidget);
+
+    HorizontalListWidget menu = new HorizontalListWidget();
+    menu.addId("remove-button-menu");
+
+    menu.addEntry(ButtonWidget.i18n("labymod.ui.button.remove", () -> {
+      this.nameTagConfiguration.getCustomTags().remove(nameTagWidget.getUserName());
+      this.nameTagWidgets.remove(nameTagWidget.getUserName());
+      this.nameTagList.getSession().setSelectedEntry(null);
+      this.setAction(null);
+    }));
+
+    menu.addEntry(ButtonWidget.i18n("labymod.ui.button.cancel", () -> this.setAction(null)));
+    this.inputWidget.addContent(menu);
+
+    return this.inputWidget;
+  }
+
+  private DivWidget initializeManageContainer(NameTagWidget nameTagWidget) {
     DivWidget inputContainer = new DivWidget();
     inputContainer.addId("input-container");
 
@@ -151,9 +162,9 @@ public class NameTagActivity extends SimpleActivity {
     iconWidget.addId("input-avatar");
     nameList.addEntry(iconWidget);
 
-    this.nameTextField = new TextFieldWidget();
-    this.nameTextField.setText(nameTagWidget.getUserName());
-    this.nameTextField.setUpdateListener(newValue -> {
+    TextFieldWidget nameTextField = new TextFieldWidget();
+    nameTextField.setText(nameTagWidget.getUserName());
+    nameTextField.setUpdateListener(newValue -> {
       if (newValue.equals(this.lastUserName)) {
         return;
       }
@@ -162,7 +173,7 @@ public class NameTagActivity extends SimpleActivity {
       iconWidget.setIcon(nameTagWidget.getIconWidget(newValue));
     });
 
-    nameList.addEntry(this.nameTextField);
+    nameList.addEntry(nameTextField);
     this.inputWidget.addContent(nameList);
 
     ComponentWidget labelCustomName = ComponentWidget.i18n("nametags.gui.manage.custom.name");
@@ -176,9 +187,9 @@ public class NameTagActivity extends SimpleActivity {
     placeHolder.addId("input-avatar");
     customNameList.addEntry(placeHolder);
 
-    this.customTextField = new TextFieldWidget();
-    this.customTextField.setText(nameTagWidget.getCustomTag().getCustomName());
-    this.customTextField.setUpdateListener(newValue -> {
+    TextFieldWidget customTextField = new TextFieldWidget();
+    customTextField.setText(nameTagWidget.getCustomTag().getCustomName());
+    customTextField.setUpdateListener(newValue -> {
       if (newValue.equals(this.lastCustomName)) {
         return;
       }
@@ -188,7 +199,7 @@ public class NameTagActivity extends SimpleActivity {
           LegacyComponentSerializer.legacyAmpersand().deserialize(newValue));
     });
 
-    customNameList.addEntry(this.customTextField);
+    customNameList.addEntry(customTextField);
     this.inputWidget.addContent(customNameList);
 
     HorizontalListWidget menu = new HorizontalListWidget();
@@ -196,17 +207,15 @@ public class NameTagActivity extends SimpleActivity {
 
     menu.addEntry(ButtonWidget.i18n("labymod.ui.button.done", () -> {
       if (nameTagWidget.getUserName().length() == 0) {
-        this.nameTagWidgets.add(nameTagWidget);
-        nameTagWidget.setActive(true);
-        this.selectedNameTag.setActive(false);
-        this.selectedNameTag = nameTagWidget;
+        this.nameTagWidgets.put(nameTextField.getText(), nameTagWidget);
+        this.nameTagList.getSession().setSelectedEntry(nameTagWidget);
       }
 
       this.nameTagConfiguration.getCustomTags().remove(nameTagWidget.getUserName());
       CustomTag customTag = nameTagWidget.getCustomTag();
-      customTag.setCustomName(this.customTextField.getText());
-      this.nameTagConfiguration.getCustomTags().put(this.nameTextField.getText(), customTag);
-      nameTagWidget.setUserName(this.nameTextField.getText());
+      customTag.setCustomName(customTextField.getText());
+      this.nameTagConfiguration.getCustomTags().put(nameTextField.getText(), customTag);
+      nameTagWidget.setUserName(nameTextField.getText());
       nameTagWidget.setCustomTag(customTag);
       this.setAction(null);
     }));
