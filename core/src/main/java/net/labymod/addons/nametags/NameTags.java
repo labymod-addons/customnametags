0dc1c4f15affc3b64767c821d9db0feb04aedba8
LabyStudio <labystudio@gmail.com>
2022-02-16 18:20:31 +0100

    update to new settings api

--- core/src/main/java/net/labymod/addons/nametags/NameTags.java
+++ core/src/main/java/net/labymod/addons/nametags/NameTags.java
@@ -1,16 +1,14 @@
 package net.labymod.addons.nametags;
 
 import com.google.inject.Singleton;
-import javax.inject.Inject;
 import net.kyori.adventure.text.Component;
 import net.labymod.addons.nametags.gui.activity.NameTagActivity;
 import net.labymod.addons.nametags.listener.ChatReceiveListener;
 import net.labymod.addons.nametags.listener.PlayerNameTagRenderListener;
 import net.labymod.api.LabyAPI;
 import net.labymod.api.client.gui.screen.widget.widgets.activity.settings.SettingOpenActivityWidget;
-import net.labymod.api.configuration.loader.ConfigurationLoader;
-import net.labymod.api.configuration.settings.SettingsRegistry;
-import net.labymod.api.configuration.settings.gui.SettingCategoryRegistry;
+import net.labymod.api.configuration.loader.impl.JsonConfigLoader;
+import net.labymod.api.configuration.settings.type.SettingRegistry;
 import net.labymod.api.event.Priority;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.lifecycle.GameInitializeEvent;
@@ -18,20 +16,19 @@ import net.labymod.api.event.client.lifecycle.GameInitializeEvent.Lifecycle;
 import net.labymod.api.event.labymod.config.ConfigurationSaveEvent;
 import net.labymod.api.event.labymod.config.SettingWidgetInitializeEvent;
 import net.labymod.api.models.addon.annotation.AddonMain;
+import javax.inject.Inject;
 
 @AddonMain
 @Singleton
 public class NameTags {
 
   private final LabyAPI labyAPI;
-  private final SettingCategoryRegistry categoryRegistry;
 
   private NameTagConfiguration configuration;
 
   @Inject
-  private NameTags(LabyAPI labyAPI, SettingCategoryRegistry categoryRegistry) {
+  private NameTags(LabyAPI labyAPI) {
     this.labyAPI = labyAPI;
-    this.categoryRegistry = categoryRegistry;
 
     labyAPI.getEventBus()
         .registerListener(this, this.labyAPI.getInjected(PlayerNameTagRenderListener.class));
@@ -50,11 +47,15 @@ public class NameTags {
       return;
     }
 
-    ConfigurationLoader configurationLoader = this.labyAPI.getConfigurationLoader();
     try {
-      this.configuration = configurationLoader.load(NameTagConfiguration.class);
-      SettingsRegistry registry = this.configuration.initializeRegistry();
-      this.categoryRegistry.register("nametags.settings", registry);
+      // Load config
+      JsonConfigLoader loader = JsonConfigLoader.createDefault();
+      this.configuration = loader.load(NameTagConfiguration.class);
+
+      // Register config
+      SettingRegistry category = SettingRegistry.namespace(this.labyAPI, this);
+      category.addSettings(this.configuration);
+      this.labyAPI.getCoreSettingRegistry().addSetting(category);
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -62,7 +63,7 @@ public class NameTags {
 
   @Subscribe
   public void onSettingWidgetInitialize(SettingWidgetInitializeEvent event) {
-    if (!event.getLayer().getId().equals("nametags.settings")) {
+    if (!event.getHolder().getPath().equals("settings.nametag")) {
       return;
     }
 
@@ -81,7 +82,7 @@ public class NameTags {
   @Subscribe
   public void onConfigurationSave(ConfigurationSaveEvent event) {
     try {
-      event.getLoader().save(this.configuration);
+      JsonConfigLoader.createDefault().save(this.configuration);
     } catch (Exception e) {
       e.printStackTrace();
     }
